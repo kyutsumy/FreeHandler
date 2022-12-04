1ad3609e4547bb2c50cc75a4ca2767441d6fa098
Kyutsumy <67562804+kyutsumy@users.noreply.github.com>
2022-12-04 01:20:21 -0300

    Add files via upload

---
+++ schemas/guild.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+/*let guild = new mongoose.Schema({
+	guild_id: String,
+  prefixo: {
+  	type: String,
+  	default: '!'
+  },
+  comandos: {
+  	type: Boolean,
+  	default: true
+  },
+  language: {
+  	type: String,
+  	default: null
+  }
+});
+
+module.exports = mongoose.model('guilds', guild)*/
